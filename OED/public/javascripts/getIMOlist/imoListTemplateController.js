79893e0de0ba8e7a4187cb3ce833f8178c0bd4bc
funone <serge-khv@yandex.ru>
2016-08-20 19:36:18 -0600

    IMO grab 1 pager

---
+++ OED/public/javascripts/getIMOlist/imoListTemplateController.js
@@ -0,0 +1,17 @@
+app.controller('imoListTemplateController', function($rootScope, $scope, $http, $location){
+  console.log('[imoListTemplateController.js] - (imoListTemplateController) - Scope is up!');
+
+  /**
+  * @desc Make a call to the server to get list of IMO
+  */
+  $scope.imoList = {};
+  $http.get('/getIMOlist/imo').success(function(data){
+    if (data.state == 'SUCCESS'){
+      console.log('To be displayed IMO');
+      $scope.imoList = data.dataArray;
+    }
+    else
+      console.log('ERROR');
+  });
+
+});
